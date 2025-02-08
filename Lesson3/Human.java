e472d0287eeb45232fc297743d03b520953e9f50
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-02-08 21:10:47 +0400

    Update Human.java

--- Lesson3/Human.java
+++ Lesson3/Human.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Human {
     public String name;
     public String surname;
@@ -8,8 +10,6 @@ public class Human {
     String[][] schedule;
     Pet pet;
 
-
-
     public Human() {}
 
     public Human(String name, String surname, int year) {
@@ -18,46 +18,57 @@ public class Human {
         this.year = year;
     }
 
-    public Human(String name, String surname, int year, Human father, Human mother)
-    {
+    public Human(String name, String surname, int year, Human father, Human mother) {
         this.name = name;
         this.surname = surname;
         this.year = year;
-        this.mother=mother;
+        this.mother = mother;
         this.father = father;
-
     }
 
-    public Human (String name, String surname, int year, int iq, Pet pet, Human mother, Human father, String[][] schedule)
-    {
-        this.name=name;
-        this.surname=surname;
-        this.year=year;
-        this.iq=iq;
-        this.mother=mother;
-        this.father=father;
+    public Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father, String[][] schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.mother = mother;
+        this.father = father;
         this.schedule = schedule;
-        this.pet=pet;
+        this.pet = pet;
     }
 
-    void greetPet()
-    {
+    void greetPet() {
         System.out.println("Hello, " + pet.nickname);
     }
 
-
-    void describePet(){
-        String sly = pet.trickLevel > 50? "very sly" : "almost not sly";
+    void describePet() {
+        String sly = pet.trickLevel > 50 ? "very sly" : "almost not sly";
         System.out.println("I have a " + pet.species + " who is " + pet.age + " years old, she is " + sly);
     }
-@Override
-public String toString() {
-    return "Human{name='" + name + "', surname='" + surname + "', year=" + year +
-            ", iq=" + iq +
-            ", mother=" + (mother != null ? mother.name : "null") +
-            ", father=" + (father != null ? father.name : "null") +
-            ", pet=" + pet + "}";
-}
 
-}
+    public boolean feedPet(boolean isTimeToFeed) {
+        if (isTimeToFeed) {
+            System.out.println("Hm... I will feed " + name + "'s " + pet.species);
+            return true;
+        } else {
+            Random random = new Random();
+            int randomValue = random.nextInt(100);
+            if (pet.trickLevel > randomValue) {
+                System.out.println("Hm... I will feed " + name + "'s " + pet.species);
+                return true;
+            } else {
+                System.out.println("I think " + name + "'s " + pet.species + " is not hungry.");
+                return false;
+            }
+        }
+    }
 
+    @Override
+    public String toString() {
+        return "Human{name='" + name + "', surname='" + surname + "', year=" + year +
+                ", iq=" + iq +
+                ", mother=" + (mother != null ? mother.name : "null") +
+                ", father=" + (father != null ? father.name : "null") +
+                ", pet=" + pet + "}";
+    }
+}
