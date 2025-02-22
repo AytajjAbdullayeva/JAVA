85773c6986695d799dcb2ee96e1cbdeafa396b68
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-02-22 21:52:40 +0400

    Update Human.java

--- hw3/Human.java
+++ hw3/Human.java
@@ -1,37 +1,52 @@
-import java.util.Map;
-import java.util.Objects;
-
-public class Human {
+class Human {
     private String name;
     private String surname;
     private int year;
-    private Map<DayOfWeek, String> schedule;
+    private int iq;
+    private Pet pet;
+    private Human mother;
+    private Human father;
+    private DayOfWeek[][] schedule;
+    private Family family;
 
-    public Human(String name, String surname, int year, Map<DayOfWeek, String> schedule) {
+    public Human(String name, String surname, int year, int iq) {
         this.name = name;
         this.surname = surname;
         this.year = year;
-        this.schedule = schedule;
+        this.iq = iq;
+        this.schedule = new DayOfWeek[7][];
+    }
+
+    public Human(String name, String surname, int year, int iq, Human mother, Human father) {
+        this(name, surname, year, iq);
+        this.mother = mother;
+        this.father = father;
+    }
+
+    public void greetPet() {
+        System.out.println("Hello, " + pet);
+    }
+
+    public void describePet() {
+        System.out.println("I have a " + pet.getClass().getSimpleName() + " named " + pet.toString());
+    }
+
+    public void feedPet(boolean isTimeToFeed) {
+        if (isTimeToFeed) {
+            System.out.println("Hm... I will feed " + pet);
+        } else {
+            System.out.println("I think " + pet + " is not hungry.");
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("Human{name='%s', surname='%s', year=%d, schedule=%s}",
-                name, surname, year, schedule);
+        return "Human{name='" + name + "', surname='" + surname + "', year=" + year + ", iq=" + iq + "}";
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Human)) return false;
-        Human human = (Human) obj;
-        return year == human.year &&
-                Objects.equals(name, human.name) &&
-                Objects.equals(surname, human.surname);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, surname, year);
+    protected void finalize() throws Throwable {
+        System.out.println("Human object is being deleted: " + this);
+        super.finalize();
     }
 }
