a54ffb775948d6b00fc5a127740cf3ddcd7291b7
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-02-26 21:19:37 +0400

    Update Vehicle.java

--- Lesson10/Vehicle.java
+++ Lesson10/Vehicle.java
@@ -17,23 +17,6 @@ abstract class Vehicle {
 }
 
 
-class Car extends Vehicle {
-    private int doors;
-
-    public Car(String brand, int speed, int doors) {
-        super(brand, speed);
-        this.doors = doors;
-    }
-
-    @Override
-    public void move() {
-        System.out.println(brand + " car moves with this  speed -->" + speed );
-    }
-
-    public void honk() {
-        System.out.println(brand + " gave a signal: Bip bip!");
-    }
-}
 
 
 class Bike extends Vehicle {
