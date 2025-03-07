68e25c995f6f0cc18e4c10a8f91ee49e4ffff277
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-03-08 00:15:28 +0400

    Update DayOfWeek.java

--- HW4/DayOfWeek.java
+++ HW4/DayOfWeek.java
@@ -1,5 +1,5 @@
 package happy_family4;
 
 enum DayOfWeek {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
 }
