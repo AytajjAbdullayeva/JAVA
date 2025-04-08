70e37d8bf2c6392e0fb037a83d986acd0243248f
AytajjAbdullayeva <abdullayeva.aytajj@gmail.com>
2025-04-08 23:15:30 +0400

    Add files via upload

---
+++ ErrorExceptionLab/Lesson6/Human.java
@@ -0,0 +1,82 @@
+package lesson6;
+
+
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Student.class, name = "student"),
+        @JsonSubTypes.Type(value = Teacher.class, name = "teacher")
+})
+public abstract class Human {
+    protected String firstName;
+    protected String lastName;
+    protected int age;
+    protected String gender;
+    protected String email;
+
+    public Human() {
+    }
+
+    public Human(String firstName, String lastName, int age, String gender, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ ErrorExceptionLab/Lesson6/Teachable.java
@@ -0,0 +1,7 @@
+package lesson6;
+
+
+
+public interface Teachable {
+    void teachCourse(Course course);
+}
